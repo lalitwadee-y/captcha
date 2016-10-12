812eb2cdb36fb7b4f1949829364673e4fb2c1a70
Lalitwadee Yodnin <57160482@go.buu.ac.th>
2016-10-12 23:57:56 +0700

    เพิ่ม class Createapp , Operator

--- spec/testcaptcha.spec.js
+++ spec/testcaptcha.spec.js
@@ -1,25 +1,40 @@
+function createApp(pattern, operator, leftOperand, rightOperand){
+  this.generate = function(){
 
 
+  }
+
+  return (Left(pattern,leftOperand),Operator(operator),Right(pattern,rightOperand))
+
+
+  //return new Captcha(pattern, operator, leftOperand, rightOperand)
+}
+
+function Operator(operator){
+  this.toString = function(){
+    if(operator === 1){
+      return '+';
+    }
+    else if(operator === 2){
+      return '-';
+    }
+  }
+}
 
 describe('Captcha App', () =>{
   describe('Pattern 1', () =>{
     const pattern = 1
 
     it('should return "1 + TWO" when in input is 1,1,1,2', () =>{
-      let app = new Captha(pattern,1,1,2)
-      expect(app.generate().toEqual('1 + TWO'))
+
+      expect(createApp(pattern, 1, 1, 2)).toEqual('1 + TWO');
     })
   })
   describe('Pattern is 2', () =>{
     const pattern = 2
 
     it('should return "THREE + 5" when in input is 2,2,3,5', () =>{
-      expect(createApp(pattern, 2, 3, 5).toEqual('THREE + 5'))
+      expect(createApp(pattern, 2, 3, 5)).toEqual('THREE + 5');
     })
   })
 })
-
-
-function createApp(pattern, operator, leftOperand, rightOperand){
-  return new Captcha(pattern, operator, leftOperand, rightOperand)
-}
