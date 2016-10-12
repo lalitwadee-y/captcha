68a73dcf31842d1e9dec8ceb983e0925d98cb683
jantana <luknum@jantanas-MacBook-Pro.local>
2016-10-13 00:10:31 +0700

    เพิ่ม class Left และ class Right

--- spec/testcaptcha.spec.js
+++ spec/testcaptcha.spec.js
@@ -20,6 +20,40 @@ function Operator(operator){
     }
   }
 }
+function Left(pattern,leftOperand){
+  this.toString = function(){
+    if(pattern === 1){
+      if(leftOperand === 0){ return '0'; }
+      else if(leftOperand === 1){ return '1';}
+      else if(leftOperand === 2){ return '2';}
+      else if(leftOperand === 3){ return '3';}
+      else if(leftOperand === 4){ return '4';}
+      else if(leftOperand === 5){ return '5';}
+      else if(leftOperand === 6){ return '6';}
+      else if(leftOperand === 7){ return '7';}
+      else if(leftOperand === 8){ return '8';}
+      else if(leftOperand === 9){ return '9';}
+    }
+  }
+}
+
+function Right(pattern,rightOperand){
+  this.toString = function(){
+
+    else if(pattern === 1){
+      if(rightOperand === 0){ return 'ZERO';}
+      else if(rightOperand === 1){ return 'ONE';}
+      else if(rightOperand === 2){ return 'TWO'; }
+      else if(rightOperand === 3){ return 'THREE';}
+      else if(rightOperand === 4){ return 'FOUR';}
+      else if(rightOperand === 5){ return 'FIVE';}
+      else if(rightOperand === 6){ return 'SIX';}
+      else if(rightOperand === 7){ return 'SEVEN';}
+      else if(rightOperand === 8){ return 'EIGHT';}
+      else if(rightOperand === 9){ return 'NINE';}
+    }
+  }
+}
 
 describe('Captcha App', () =>{
   describe('Pattern 1', () =>{
