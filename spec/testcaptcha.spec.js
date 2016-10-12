c9d05ad02d4fa7de6f7f16959349c59ea8550ec3
Lalitwadee Yodnin <57160482@go.buu.ac.th>
2016-10-12 22:56:23 +0700

    test commit

--- spec/testcaptcha.spec.js
+++ spec/testcaptcha.spec.js
@@ -1,7 +1,25 @@
+
+
+
 describe('Captcha App', () =>{
   describe('Pattern 1', () =>{
+    const pattern = 1
+
     it('should return "1 + TWO" when in input is 1,1,1,2', () =>{
+      let app = new Captha(pattern,1,1,2)
       expect(app.generate().toEqual('1 + TWO'))
     })
   })
+  describe('Pattern is 2', () =>{
+    const pattern = 2
+
+    it('should return "THREE + 5" when in input is 2,2,3,5', () =>{
+      expect(createApp(pattern, 2, 3, 5).toEqual('THREE + 5'))
+    })
+  })
 })
+
+
+function createApp(pattern, operator, leftOperand, rightOperand){
+  return new Captcha(pattern, operator, leftOperand, rightOperand)
+}
