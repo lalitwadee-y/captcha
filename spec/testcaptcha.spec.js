2404111a871504206c5957a98aa84edcb1516372
jantana <luknum@jantanas-MacBook-Pro.local>
2016-10-13 18:00:46 +0700

    เพิ่ม test 1,1,1,0

--- spec/testcaptcha.spec.js
+++ spec/testcaptcha.spec.js
@@ -75,6 +75,9 @@ describe('Captcha App', () =>{
     it('should return "0 + ZERO" when in input is 1,1,0,0', () =>{
       expect(createApp(pattern, 1, 0, 0)).toEqual('0 + ZERO');
     })
+    it('should return "1 + ZERO" when in input is 1,1,1,0', () =>{
+      expect(createApp(pattern, 1, 1, 0)).toEqual('1 + ZERO');
+    })
   })
   describe('Pattern is 2', () =>{
     const pattern = 2
